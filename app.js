720ec9b377b6d3262becd1d1ef6d505c7b172c25
giannifontanot <giannifontanot@gmail.com>
2021-08-10 21:14:09 -0500

    Sever listening. Students model generated.

--- app.js
+++ app.js
@@ -1,42 +1,17 @@
 const express = require('express');
-const {sequelize, Student} = require('./models/index');
-const PORT = process.env.PORT || 3001;
-
+const routes = require('./routes');
+const sequelize = require('./config/connection');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/student', async (req, res) => {
-    try {
-        const {id, firstName, lastName} = req.body;
-        const student = await Student.create({id, firstName, lastName});
-        console.log("student: " + JSON.stringify(student));
+// turn on routes
+app.use(routes);
 
-        res.status(202).json(student);
-
-    } catch (err) {
-        res.status(500).send(">>>>>" + err.message);
-    }
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
-
-app.get('/allStudents', (req, res) => {
-    try {
-        const students = Student.findAll();
-        return res.status(200).json(students);
-    } catch (e) {
-        return res.status(500).send(e.message);
-    }
-})
-
-app.listen({port: 3000}, async () => {
-
-    //lets check the connection with MySQL
-    // await sequelize.authenticate();
-    console.log('authenticated to MySQL');
-
-    await sequelize.sync({force: true});
-    console.log('Database sync complete');
-});
-
-
-

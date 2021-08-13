9fc67ec2938637bf1d35c9cb14c441d37ff9da50
Brian OConnor <boconnor20@gmail.com>
2021-08-12 21:00:17 -0500

    updated form.handlebars, studentForm.js to link, later connection to api/behavior route

--- public/js/studentForm.js
+++ public/js/studentForm.js
@@ -1,1 +1,20 @@
-
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const studentBehavior = document.querySelector('input[name="behavior-form"]').value;
+    const behaviorContent = document.querySelector('input[name="behavior content"]').value;
+  
+    const response = await fetch(`/api/behavior`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Behavior Content-Type': 'application/json'
+      }
+    });
+  
+  };
+  
+document.querySelector('#new-behavior-form').addEventListener('submit', newFormHandler);

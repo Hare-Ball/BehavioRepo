75bbd895df786936c55d82e30eacac7a24f13ec1
Hare-Ball <robby.hare@gmail.com>
2021-08-14 16:35:50 -0500

    got the student route working and got Handlebars to work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,25 +2,41 @@ const router = require('express').Router();
 const { Student, Behavior } = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-        const studentData = await Student.findAll();
-        const students = studentData.map((student) => student.get({ plain: true }));
-        res.render('homepage', {students});
+    const studentData = await Student.findAll().catch((err) => { 
+        res.json(err);
+      });
+      // We use map() to iterate over dishData and then add .get({ plain: true }) each object to serialize it. 
+      const students = studentData.map((student) => student.get({ plain: true }));
+      // We render the template, 'all', passing in dishes, a new array of serialized objects.
+      res.render('students', { students });
+      });
+//     try {
+//         const studentData = await Student.findAll();
+//         res.status(200).json(studentData)
+//         // console.log(studentData)
+//         const students = studentData.map((student) => student.get({ plain: true }));
 
-    } catch (err) {
-    res.status(500).json(err);
-    }
-});
+//         // res.sendFile(path.join(__dirname, '../views/layouts/main.handlebars'));
+//         res.render('students', {
+//             students});
 
-router.get('/student/:id', async (req, res) => {
-    try {
-        const studentData = await Student.findByPk(req.params.id);
-        const student = studentData.get({ plain: true });
+//     } catch (err) {
+//     res.status(500).json(err);
+//     }
 
-        res.render('student', {
-        ...student,
-        })
-    } catch {
-        res.status(500).json(err);  
-    }
-});
+// });
+
+// router.get('/student/:id', async (req, res) => {
+//     try {
+//         const studentData = await Student.findByPk(req.params.id);
+//         const student = studentData.get({ plain: true });
+
+//         res.render('students', {
+//         ...student,
+//         })
+//     } catch {
+//         res.status(500).json(err);  
+//     }
+// });
+
+module.exports = router;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,2 @@
-
 DROP DATABASE IF EXISTS student_behavior_db;
 CREATE DATABASE student_behavior_db;

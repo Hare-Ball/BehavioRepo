19318f3564d9fed7516afa9c46279c192844703f
giannifontanot <giannifontanot@gmail.com>
2021-08-17 18:48:55 -0500

    App.js prepared to Seed db and then run the server.

--- app.js
+++ app.js
@@ -10,17 +10,18 @@ app.use(express.json());
 app.use(router);
 
 const run = async () => {
-
+    console.log("---> JAWSDB_URL :" + process.env.JAWSDx_URL);
     const resultSynch = await sequelize.sync({force: true});
     const resultSow = await peasant.sow();
     const resultServerStart = await app.listen(PORT, () => {
-        console.log('Sever running on port: %j', PORT);
-        console.log('http://localhost:%j/', PORT);
-        console.log('http://localhost:%j/api/', PORT);
         console.log('http://localhost:%j/api/Allstudents', PORT);
-        console.log('http://localhost:%j/api/insertStudent', PORT);
     });
 
+    console.log('')
+        console.log('Sever running on port: %j', PORT);
+    console.log('')
+    console.log('Database Seeded.')
+    console.log('')
 
     console.log("---> resultSynch :" + resultSynch);
     console.log("---> resultSow :" + resultSow);

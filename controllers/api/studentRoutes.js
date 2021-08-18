84b879464bc4611b0b1807994146c85a1f39075d
Hare-Ball <robby.hare@gmail.com>
2021-08-18 18:17:43 -0500

    updates to routes in API for students and behaviors

--- controllers/api/studentRoutes.js
+++ controllers/api/studentRoutes.js
@@ -1,86 +1,38 @@
 const router = require('express').Router();
-const { Student, Project, User } = require('../../models');
+const { Student} = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-    //find all students
-    try{
-      const studentData = await Student.findAll({
-        //include: user?
-      });
-      res.status(200).json(studentData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Student.create({
+      ...req.body,
+      user_id: req.session.user_id,
     });
 
-router.get('/:id', async (req, res) => {
-    // find one student by its `id` value
-
-    try {
-      const studentData = await Student.findByPk(req.params.id, {
-        //include: students?
-      });
-  
-      if (!studentData) {
-        res.status(404).json({ message: 'No Student found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(studentData);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-
-router.post('/', async (req, res) => {
-  // create a new student
-   try {
-    const studentData = await Student.create(req.body);
-     res.status(200).json(studentData);
-   } catch (err) {
+    res.status(200).json(newStudent);
+  } catch (err) {
     res.status(400).json(err);
- }
- });
+  }
+});
 
- router.put('/:id', (req, res) => {
-    // update a Student by its `id` value
-    Student.update(
-    //   {
-    //    id:req.body.id,
-    //    category_name: req.body.category_name 
-    //   },
-    //   {
-    //     where: {
-    //       id: req.params.id
-    //     }
-    //   }
-    )
-    .then((updatedStudent) => {
-      res.json(updatedStudent);
-    })
-    .catch((err) => res.json(err));
-  });
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const studentData = await Student.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
 
-  router.delete('/:id', async (req, res) => {
-    // delete a Student by its `id` value
-    try {
-      const studentData = await Student.destroy({
-        where: {
-          id: req.params.id
-        }
-      });
-  
-      if (!studentData) {
-        res.status(404).json({ message: 'No Student found with this id!' });
-        return;
-      }
-  
-      res.status(200).json(studentData);
-    } catch (err) {
-      res.status(500).json(err);
+    if (!studentData) {
+      res.status(404).json({ message: 'No student found with this id!' });
+      return;
     }
-  });
-  
-  module.exports = router;
-  
 
+    res.status(200).json(studentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;

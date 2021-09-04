915dd83eb982e7847f43ab7b29d67c61aefd8352
Thomas Kang <tom.k.500587@gmail.com>
2021-09-04 02:04:18 -0500

    working on routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,91 +4,97 @@ const router = require('express').Router();
 const { Student, Teacher, Behavior, BehaviorNote } = require('../models');
 // const withAuth = require('../utils/auth');
 
-// route to show all students
+// route to teacher all 
 router.get('/', async (req, res) => {
   Teacher.findAll({
+    attributes: ['id', 'teacherName', 'grade', 'email'],
+  }).then (teacherTable => res.json(teacherTable))
+  .catch (err => {
+      console.error(err);
+      res.status(500).json(err);
+  });
+});
+
+
+    
+router.get('/teacher/:id', (req,res)=> {
+  Teacher.findOne({
+    where: {
+      id: req.params.id
+    },
     attributes: ['id', 'teacherName', 'grade', 'email'],
     include: [
       {
         model: Student,
-        attributes: ['id','student_name', 'student_grade']
-
+        attributes: ['id','student_name', 'student_grade'],
+        include: {
+            model: BehaviorNote,
+            attributes: ['id', 'behavior_note', 'date_created'],
+        }
+      },
+      {
+        model: Behavior,
+        attributes: ['behavior'],
+        through: BehaviorNote,
+        as: 'behavior'
       }
     ]
-  }).then (studentTable => {
-    const students = studentTable.map(student => student.get({ plain: true }));
-    res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });   
-
-  });
-
-
-    
-
-
-router.get('/teacher/:id', async (req,res)=> {
- try{
-    const teacherTable = await Teacher.findByPk(req.params.id);
-    const teacher = teacherTable.get({ plain: true });
-
-    res.render('profile', {
-  ...teacher,
-    })
+  }).then (teacherTable => {
+    const teachers = teacherTable.map(teacher => teacher.get({ plain: true }));
+    res.render('homepage', { teachers, loggedIn: req.session.loggedIn });
   
-} catch {
-    res.status(500).json(err);  
-}
+}).catch(err => {
+  console.log(err);
+  res.status(500).json(err);  
 });
-
-router.get('/student', async (req, res) => {
-  try {
-    const studentTable = await Student.findAll({}).catch((err) => { 
-      res.json(err);
-    });
-    const students = studentTable.map((student) => student.get({ plain: true }));
-      res.render('students', {
-      ...students,
-      })
-  } catch {
-      res.status(500).json(err);  
-  }
 });
 
 
-router.get('/student/:id', async (req, res) => {
-    try {
-        const studentTable = await Student.findByPk(req.params.id);
-        const student = studentTable.get({ plain: true });
+// router.get('/student', async (req, res) => {
+//   try {
+//     const studentTable = await Student.findAll({}).catch((err) => { 
+//       res.json(err);
+//     });
+//     const students = studentTable.map((student) => student.get({ plain: true }));
+//       res.render('students', {
+//       ...students,
+//       })
+//   } catch {
+//       res.status(500).json(err);  
+//   }
+// });
 
-        res.render('students', {
-        ...student,
-        })
-    } catch {
-        res.status(500).json(err);  
-    }
-});
 
-router.get('/profile', async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const teacherTable = await Teacher.findAll({}).catch((err)=> {
+// router.get('/student/:id', async (req, res) => {
+//     try {
+//         const studentTable = await Student.findByPk(req.params.id);
+//         const student = studentTable.get({ plain: true });
 
-    });
+//         res.render('students', {
+//         ...student,
+//         })
+//     } catch {
+//         res.status(500).json(err);  
+//     }
+// });
 
-    const teacher = teacherTable.get({ plain: true });
-    console.log(teacher)
-    res.render('profile', {
-      ...teacher,
-      // logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// router.get('/profile', async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const teacherTable = await Teacher.findAll({}).catch((err)=> {
+
+//     });
+
+//     const teacher = teacherTable.get({ plain: true });
+//     console.log(teacher)
+//     res.render('profile', {
+//       ...teacher,
+//       // logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 //Trying to get behaviors to show on student page
 // router.get('/student/:id', async (req, res) => {
@@ -102,7 +108,7 @@ router.get('/profile', async (req, res) => {
 
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-      res.redirect('/students');
+      res.redirect('/');
       return;
     }
   
